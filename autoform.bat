5c03fc482bd4595ce89c65ed59c6f73b4097225f
Zaim Kholilurohim Ramadhan <142579599+Kazamn@users.noreply.github.com>
2026-08-22 22:17:38 +0700

    Update autoform.bat

--- autoform.bat
+++ autoform.bat
@@ -3,9 +3,7 @@ title Autoform TAK Automation
 
 echo Menjalankan sistem otomatisasi TAK...
 
-:: Otomatis pindah ke direktori tempat file .bat ini berada
 cd /d "%~dp0"
 
-:: Menjalankan script Python
 python autoform.py
 pause
